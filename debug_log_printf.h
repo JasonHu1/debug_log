7da8af6742405bd8f739fe9c6ce8f233a991f564
qigong.hu@tuya.com <qigong.hu@tuya.com>
2021-08-11 10:16:07 +0800

    add ziroom openwrt log style

--- debug_log_printf.h
+++ debug_log_printf.h
@@ -328,4 +328,34 @@ extern void printRemainHeap(const char*functionName,int line,char*a);
 #define LWIP_DEBUGF(debug, message) 
 #endif /* LWIP_DEBUG */
 #endif
-#endif
+/****************************************************************************/
+/***       openwrt                                            ***/
+/****************************************************************************/
+enum{
+    LOG_ERROR,
+    LOG_WARNING,
+    LOG_INFO,
+    LOG_DEBUG,
+    LOG_MSGDUMP,
+    LOG_EXCESSIVE,
+};
+//general simple
+#define vLOG_DEBUG(...)    do{if(g_debug_level>=LOG_DEBUG){ printf(__VA_ARGS__);}}while(0)
+#define vLOG_INFO(...)     do{if(g_debug_level>=LOG_INFO){ printf(__VA_ARGS__);}}while(0)
+#define vLOG_ERR(...)      do{if(g_debug_level>=LOG_ERROR){printf(__VA_ARGS__);}}while(0)
+#define vLOG_WARN(...)     do{if(g_debug_level>=LOG_WARNING){printf(__VA_ARGS__);}}while(0)
+
+//module detailedly
+#define vLOG_APP(level,_fmt_,...)\
+    do{if(g_debug_level>=level){printf("APP::[%s]<%d>"_fmt_"\n\r",__FUNCTION__,__LINE__,##__VA_ARGS__);}}while(0)
+    
+#define vLOG_MODULE1(level,_fmt_,...)\
+    do{if(g_debug_level>=level){printf("MODULE1::[%s]<%d>"_fmt_"\n\r",__FUNCTION__,__LINE__,##__VA_ARGS__);}}while(0)
+    
+#define vLOG_MODULE2(level,_fmt_,...)\
+    do{if(g_debug_level>=level){printf("MODULE2::[%s]<%d>"_fmt_"\n\r",__FUNCTION__,__LINE__,##__VA_ARGS__);}}while(0)
+    
+#define vLOG_MODULE3(level,_fmt_,...)\
+    do{if(g_debug_level>=level){printf("MODULE3::[%s]<%d>"_fmt_"\n\r",__FUNCTION__,__LINE__,##__VA_ARGS__);}}while(0)
+    
+#endif//__DEBUG_LOG_PRINTF_H__
